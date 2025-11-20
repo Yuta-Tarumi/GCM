e6513c45fbf07623d0247a8c459fc17c9f2486e2
Yuta-Tarumi <33238523+Yuta-Tarumi@users.noreply.github.com>
2025-11-20 15:03:40 +0900

    Remove test suites and document minimal core

--- afes_venus_jax/tests/test_driver.py
+++
@@ -1,8 +0,0 @@
-from afes_venus_jax import driver
-
-
-def test_driver_run_returns_dataset():
-    ds = driver.run(nsteps=3, history_stride=1)
-    assert "zeta" in ds
-    assert ds.dims["lat"] > 0 and ds.dims["lon"] > 0
-    assert ds.dims["time"] >= 1
